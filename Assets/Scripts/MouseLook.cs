843e63c7a6da454eb14a80e7b3d84b35e39b95d2
William <willcallihan13@gmail.com>
2022-04-14 09:46:49 -0500

    created testing level and basic first-person character

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+    [SerializeField] Transform playerBody;
+    [SerializeField] private float mouseSensitivity = 100f;
+
+    private float xRotation = 0f;
+
+    void Update() {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //clamps the up and down rotation between -90 and 90 degrees
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //looks up and down
+        playerBody.Rotate(Vector3.up * mouseX); //rotates the entire player
+    }
+}
---
+++ Assets/Scripts/TracerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class TracerMovement : MonoBehaviour {
+
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private AudioClip jumpSound;
+
+    private CharacterController characterControler;
+    private AudioSource audioSource;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    void Awake() {
+        characterControler = GetComponent<CharacterController>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        //check if on ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.6f, groundMask);
+        if(isGrounded && velocity.y < 0) {
+            velocity.y = -2f; //just makes sure the player sticks to the ground
+        }
+
+        //take in inputs
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+
+        //use character controller to move
+        Vector3 moveVector = (transform.right * inputX) + (transform.forward * inputZ);
+        if(moveVector.magnitude > 1) {
+            moveVector /= moveVector.magnitude; //normalizes magnitude so the character doesn't move faster when moving diagonally
+        }
+        characterControler.Move(moveVector * speed * Time.deltaTime);
+
+        //jump
+        if(Input.GetButtonDown("Jump") && isGrounded) {
+            //jump upwards
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            //play jump sound effect
+            audioSource.PlayOneShot(jumpSound);
+        }
+
+        //simulate gravity
+        velocity.y += gravity * Time.deltaTime;
+        characterControler.Move(velocity * Time.deltaTime);
+    }
+}
